91809473454c90df8baa138f3ebbf54b559d557a
gurpartb <gurpartap.s.bhatti@gmail.com>
2018-12-31 17:34:22 -0800

    added logistic regression template

---
+++ Machine-Learning-A-Z/Part 3 - Classification/Section 14 - Logistic Regression/logistic_regression_g.R
@@ -0,0 +1,17 @@
+# Logistic Regression
+
+# Importing the dataset
+dataset = read.csv('Social_Network_Ads.csv')
+dataset = dataset[, 3:5]
+
+# Splitting the dataset into the Training set and Test set
+# install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Purchased, SplitRatio = 0.75)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+# Feature Scaling
+training_set[, 1:2] = scale(training_set[, 1:2])
+test_set[, 1:2] = scale(test_set[, 1:2])
